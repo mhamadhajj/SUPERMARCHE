b57d3b599b1b58454f7284000455ae644804deaf
mhamadhajj <93221918+mhamadhajj@users.noreply.github.com>
2022-02-15 13:03:10 +0100

    HOME

---
+++ home.js
@@ -0,0 +1,40 @@
+ let change=document.getElementById("bigger");
+change.addEventListener("mouseover",function(){
+	change.style.fontSize="40px";
+	change.style.color="#18689D";
+});
+ let auto=document.getElementById("bigger");
+auto.addEventListener("mouseout",function(){
+	auto.style.fontSize="20px";
+	auto.style.color="black";
+});
+ let change1=document.getElementById("bigger1");
+change1.addEventListener("mouseover",function(){
+	change1.style.fontSize="30px";
+	change1.style.color="#18689D";
+});
+ let auto1=document.getElementById("bigger1");
+auto1.addEventListener("mouseout",function(){
+	auto1.style.fontSize="20px";
+	auto1.style.color="black";
+});
+ let change2=document.getElementById("bigger2");
+change2.addEventListener("mouseover",function(){
+	change2.style.fontSize="30px";
+	change2.style.color="#18689D";
+});
+ let auto2=document.getElementById("bigger2");
+auto2.addEventListener("mouseout",function(){
+	auto2.style.fontSize="20px";
+	auto2.style.color="black";
+});
+ let change3=document.getElementById("bigger3");
+change3.addEventListener("mouseover",function(){
+	change3.style.fontSize="30px";
+	change3.style.color="#18689D";
+});
+ let auto3=document.getElementById("bigger3");
+auto3.addEventListener("mouseout",function(){
+	auto3.style.fontSize="20px";
+	auto3.style.color="black";
+});
